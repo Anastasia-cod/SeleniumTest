d0c5df8fc1932de078fbeabca5d7a7066a3be4bc
admin <nastyabonybelay@gmail.com>
2023-04-15 11:05:11 +0300

    add part 1

---
+++ SeleniumTest/AddRemoveElementsTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace SeleniumTest
+{
+    [TestFixture]
+
+    public class AddRemoveElementsTests
+    {
+        WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [Test]
+        public void CheckCountOfElements()
+        {
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/add_remove_elements/");
+            IWebElement addElement = ChromeDriver.FindElement(By.XPath("//button[text()='Add Element']"));
+            addElement.Click();
+            addElement.Click();
+
+            IWebElement removeElement = ChromeDriver.FindElement(By.XPath("//button[text()='Delete']"));
+            removeElement.Click();
+
+            var addedElement = ChromeDriver.FindElements(By.ClassName("added-manually"));
+
+            var actualCount = addedElement.Count();
+            var expectedCount = 1;
+
+            Assert.That(expectedCount, Is.EqualTo(actualCount), $"Actual added elements: {actualCount}, Expected added elements: {expectedCount}");           
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
+        }   
+    }
+}
+
