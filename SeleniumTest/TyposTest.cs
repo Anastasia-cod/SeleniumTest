80c68d52ad190ed424e3644c8482afd601f17df3
admin <nastyabonybelay@gmail.com>
2023-04-15 22:57:23 +0300

    add typos test

---
+++ SeleniumTest/TyposTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support;
+
+namespace SeleniumTest
+{
+    [TestFixture]
+
+    public class TyposTest
+    {
+        WebDriver ChromeDriver { get; set; }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ChromeDriver = new ChromeDriver();
+            ChromeDriver.Manage().Window.Maximize();
+            ChromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(15);
+        }
+
+        [Test]
+        public void Typos_CheckSpelling()
+        {
+            ChromeDriver.Navigate().GoToUrl("http://the-internet.herokuapp.com/typos");
+            var text = ChromeDriver.FindElement(By.XPath($"//p[2]"));
+            var textActual = text.Text;
+
+            Assert.That(textActual, Is.EqualTo("Sometimes you'll see a typo, other times you won't."));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            ChromeDriver.Quit();
+        }
+    }
+}
+
